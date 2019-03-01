a79c428e31ec616030433f61ba199ee4ff26971d
Chris Berthiaume <chrisbee@uw.edu>
2019-02-28 17:06:02 -0800

    Improve CSV ingest workflow tools

--- makedb.sh
+++ makedb.sh
@@ -6,7 +6,7 @@
 # * environment variables PGHOST, PGUSER, PGPASSWORD
 
 # Specify name for new database here
-DBNAME=gradients2
+DBNAME=gradients2_dev
 
 # Make the database
 # Bit of a hack to do "IF NOT EXISTS"
@@ -20,51 +20,51 @@ psql "$DBNAME" <<EOF
 CREATE EXTENSION IF NOT EXISTS timescaledb CASCADE;
 EOF
 
-# Make SeaFlow table
-#./csv2table.py --text-columns cruise,file,pop seaflow \
-#  instrument-files/seaflow_MGL1704/prelim-stat.csv
+# # Make SeaFlow table
+# #./csv2table.py --text-columns cruise,file,pop seaflow \
+# #  instrument-files/seaflow_MGL1704/prelim-stat.csv
 
-psql "$DBNAME" <<EOF
-CREATE TABLE IF NOT EXISTS seaflow (
-  cruise TEXT,
-  file TEXT,
-  time TIMESTAMPTZ NOT NULL,
-  lat DOUBLE PRECISION,
-  lon DOUBLE PRECISION,
-  opp_evt_ratio DOUBLE PRECISION,
-  flow_rate DOUBLE PRECISION,
-  file_duration DOUBLE PRECISION,
-  pop TEXT,
-  n_count DOUBLE PRECISION,
-  abundance DOUBLE PRECISION,
-  fsc_small DOUBLE PRECISION,
-  chl_small DOUBLE PRECISION,
-  pe DOUBLE PRECISION
-);
-SELECT create_hypertable('seaflow', 'time');
-EOF
+# psql "$DBNAME" <<EOF
+# CREATE TABLE IF NOT EXISTS seaflow (
+#   cruise TEXT,
+#   file TEXT,
+#   time TIMESTAMPTZ NOT NULL,
+#   lat DOUBLE PRECISION,
+#   lon DOUBLE PRECISION,
+#   opp_evt_ratio DOUBLE PRECISION,
+#   flow_rate DOUBLE PRECISION,
+#   file_duration DOUBLE PRECISION,
+#   pop TEXT,
+#   n_count DOUBLE PRECISION,
+#   abundance DOUBLE PRECISION,
+#   fsc_small DOUBLE PRECISION,
+#   chl_small DOUBLE PRECISION,
+#   pe DOUBLE PRECISION
+# );
+# SELECT create_hypertable('seaflow', 'time');
+# EOF
 
-# Make nav table
-#./csv2table.py nav instrument-files/nav.csv
+# # Make nav table
+# #./csv2table.py nav instrument-files/nav.csv
 
-psql "$DBNAME" <<EOF
-CREATE TABLE IF NOT EXISTS nav (
-  time TIMESTAMPTZ NOT NULL,
-  lat DOUBLE PRECISION,
-  lon DOUBLE PRECISION
-);
-SELECT create_hypertable('nav', 'time');
-EOF
+# psql "$DBNAME" <<EOF
+# CREATE TABLE IF NOT EXISTS nav (
+#   time TIMESTAMPTZ NOT NULL,
+#   lat DOUBLE PRECISION,
+#   lon DOUBLE PRECISION
+# );
+# SELECT create_hypertable('nav', 'time');
+# EOF
 
-# Make par table
-#./csv2table.py par instrument-files/par.csv
+# # Make par table
+# #./csv2table.py par instrument-files/par.csv
 
-psql "$DBNAME" <<EOF
-CREATE TABLE IF NOT EXISTS par (
-  time TIMESTAMPTZ NOT NULL,
-  par DOUBLE PRECISION,
-  temp DOUBLE PRECISION,
-  salinity DOUBLE PRECISION
-);
-SELECT create_hypertable('par', 'time');
-EOF
+# psql "$DBNAME" <<EOF
+# CREATE TABLE IF NOT EXISTS par (
+#   time TIMESTAMPTZ NOT NULL,
+#   par DOUBLE PRECISION,
+#   temp DOUBLE PRECISION,
+#   salinity DOUBLE PRECISION
+# );
+# SELECT create_hypertable('par', 'time');
+# EOF
---
+++ toml2sql.py
@@ -0,0 +1,89 @@
+#!/usr/bin/env python
+import click
+import os
+import sys
+import toml
+
+lu = { 'text': 'TEXT', 'real': 'DOUBLE PRECISION', 'time': 'TIMESTAMPTZ NOT NULL' }
+
+@click.command()
+@click.option('--geo-join/--no-geo-join', default=True)
+@click.argument('input', type=click.Path(exists=True), nargs=1)
+def main(geo_join, input):
+    with open(input, newline=None, encoding='utf-8') as f:
+        toml_text = f.read()
+    data = toml.loads(toml_text)
+    print(create_table(data))
+    print('')
+    print(create_time_bucket_view(data))
+    if geo_join:
+        print('')
+        print(create_geo_join_view(data))
+
+
+def create_table(data):
+    sql_lines = ['CREATE TABLE IF NOT EXISTS {} ('.format(data['table'])]
+    for i, f in enumerate(data['fields']):
+        sql_lines.append('  {} {}'.format(f['name'], lu[f['type']]))
+        if i < len(data['fields']) - 1:
+            sql_lines[-1] += ','
+    sql_lines.append(');')
+    sql_lines.append('')
+    sql_lines.append("SELECT create_hypertable('{}', 'time', if_not_exists := true);".format(data['table']))
+    return os.linesep.join(sql_lines)
+
+
+def create_time_bucket_view(data, bucket_width='15m'):
+    real_fields = [x['name'] for x in data['fields'] if x['type'] == 'real']
+    group_fields = [x['name'] for x in data['fields'] if x['type'] == 'text' and x['groupby']]
+    sql_lines = ['CREATE OR REPLACE VIEW {}_{} AS'.format(data['table'], bucket_width)]
+    sql_lines.append('  SELECT')
+    sql_lines.append("    time_bucket('{}', {}.time) AS time".format(bucket_width, data['table']))
+
+    for i, gf in enumerate(group_fields):
+        sql_lines[-1] += ','
+        sql_lines.append(f'    {gf}')
+
+    for i, rf in enumerate(real_fields):
+        sql_lines[-1] += ','
+        sql_lines.append(f'    avg({rf}) as {rf}')
+    
+    sql_lines.append('  FROM {}'.format(data['table']))
+    sql_lines.append('  GROUP BY 1')
+    if group_fields:
+        for i in range(len(group_fields)):
+            sql_lines[-1] += f', {i+2}'
+    sql_lines.append('  ORDER BY 1;')
+    return os.linesep.join(sql_lines)
+
+
+def create_geo_join_view(data, bucket_width='15m'):
+    real_fields = [x['name'] for x in data['fields'] if x['type'] == 'real']
+    group_fields = [x['name'] for x in data['fields'] if x['type'] == 'text' and x['groupby']]
+    sql_lines = ['CREATE OR REPLACE VIEW {}_{}_geo AS'.format(data['table'], bucket_width)]
+    sql_lines.append('  SELECT')
+    sql_lines.append("    a.time")
+    for i, rf in enumerate(real_fields):
+        sql_lines[-1] += ','
+        if rf == 'lat' or rf == 'lon':
+            sql_lines.append('    a.{} AS {}_{}'.format(rf, data['table'], rf))
+        else:
+            sql_lines.append(f'    a.{rf}')
+
+    for i, gf in enumerate(group_fields):
+        sql_lines[-1] += ','
+        sql_lines.append(f'    a.{gf}')
+
+    sql_lines[-1] += ','
+    sql_lines.append('    b.lat')
+    sql_lines[-1] += ','
+    sql_lines.append('    b.lon')
+    sql_lines.append('  FROM {}_{} AS a'.format(data['table'], bucket_width))
+    sql_lines.append('  INNER JOIN geo_{} AS b'.format(bucket_width))
+    sql_lines.append('  ON a.time = b.time')
+    sql_lines.append('  ORDER BY 1;')
+    return os.linesep.join(sql_lines)
+
+
+if __name__ == '__main__':
+    main()
