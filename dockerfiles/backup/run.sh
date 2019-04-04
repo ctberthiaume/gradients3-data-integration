ae8ba31ba5c5edd5ada3c271dd08062513f7d586
Chris Berthiaume <chrisbee@uw.edu>
2019-04-04 14:53:49 -0700

    Add secrets-loader script to backup service

--- dockerfiles/backup/run.sh
+++ dockerfiles/backup/run.sh
@@ -9,38 +9,9 @@
 # BORG_REPO (location of Borg repo, could be a mounted volume)
 # FILECACHE_DIR (directory / volume mount point to store minio cached files)
 
-# Taken from postgres docker startup script to handle
-# docker secrets from file.
-# https://github.com/docker-library/postgres/blob/ef04f3055bab11b10d3d5c41a659acfacf2c850b/10/docker-entrypoint.sh
-#
-# usage: file_env VAR [DEFAULT]
-#    ie: file_env 'XYZ_DB_PASSWORD' 'example'
-# (will allow for "$XYZ_DB_PASSWORD_FILE" to fill in the value of
-#  "$XYZ_DB_PASSWORD" from a file, especially for Docker's secrets feature)
-file_env() {
-    local var="$1"
-    local fileVar="${var}_FILE"
-    local def="${2:-}"
-    if [ "${!var:-}" ] && [ "${!fileVar:-}" ]; then
-        echo >&2 "error: both $var and $fileVar are set (but are exclusive)"
-        exit 1
-    fi
-    local val="$def"
-    if [ "${!var:-}" ]; then
-        val="${!var}"
-    elif [ "${!fileVar:-}" ]; then
-        val="$(< "${!fileVar}")"
-    fi
-    export "$var"="$val"
-    unset "$fileVar"
-}
-
-# Environment variables for secrets
-# MINIO_ACCESS_KEY_FILE or MINIO_ACCESS_KEY
-# MINIO_SECRET_KEY_FILE or MINIO_SECRET_KEY
-file_env "BORG_PASSPHRASE"  # or use BORG_PASSCOMMAND
-file_env "MINIO_ACCESS_KEY"
-file_env "MINIO_SECRET_KEY"
+# Load secrets from files with this script in Docker secrets
+# fashion.
+. /app/load_secrets.sh
 
 # Where we cache files
 [ -n "$FILECACHE_DIR" ] && mkdir "$FILECACHE_DIR" 2>/dev/null
